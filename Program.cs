75c19afd42fa3f11b24a067f0438345038b19a38
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 18:50:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_12__If_StatementConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         bool isMale = true;
+         if (isMale) 
+         {
+                Console.WriteLine("You are male");
+
+          }
+         else
+          { 
+               Console.WriteLine("You are female!");
+            
+          }
+         bool isTall = false;
+            if (!isTall && isMale)
+         {
+            Console.WriteLine("You are not tall, but you are male.");
+         }
+         else
+         {
+             Console.WriteLine("You are either not tall or not a male.");
+
+          }
+
+            Console.Read();
+            
+
+        }
+    }
+}
